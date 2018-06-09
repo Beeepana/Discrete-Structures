aa4782906b1e983c7ab5e7ecaa30fe0fff52215f
Beeepana <beeepanathakuri@gmail.com>
2018-06-09 18:03:46 +0545

    floor and ceiling using function

---
+++ Find floor and ceiling of a number using function..cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float n;
+	cout<<"enter a number:";
+	cin>>n;
+	cout<<"ceiling is :"<<ceil(n)<<endl;
+	cout<<"floor is:"<<floor(n)<<endl;
+	
+}
